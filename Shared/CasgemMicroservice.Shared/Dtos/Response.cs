843344c4893788a8e13192042c04fcfd79ab0726
Sumeyye <sumeyyesabirli@gmail.com>
2023-08-08 10:32:20 +0300

    Add project files.

---
+++ Shared/CasgemMicroservice.Shared/Dtos/Response.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Shared.Dtos
+{
+    public class Response <T>
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; set; }
+        public bool IsSuccessful { get; set; }
+        public List<string> Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
+
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T>
+            {
+                Data = default(T),
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+        
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                IsSuccessful = false,
+                StatusCode = statusCode
+            };
+        }
+
+        public static Response<T> Fail(string errors, int statusCode)
+        {
+            return new Response<T> {
+
+                IsSuccessful = false,
+                Errors = new List<string>() { errors },
+                StatusCode = statusCode 
+            };
+        }
+    }
+}
